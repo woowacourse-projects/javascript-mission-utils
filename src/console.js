ecc37dc21d741f8f55cbc4fbbeed745300b3977e
Cron <hojoong.choi@woowahan.com>
2023-09-20 11:48:57 +0900

    temporary: 라이브러리 테스트를 위한 임시 커밋

--- src/console.js
+++ src/console.js
@@ -1,51 +1,58 @@
 import readline from "readline";
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
 class Console {
   constructor() {}
 
   static readLine(query, callback) {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
     if (arguments.length !== 2) {
-        throw new Error("arguments must be 2.");
+      throw new Error("arguments must be 2.");
     }
 
     if (typeof query !== "string") {
-        throw new Error("query must be string");
+      throw new Error("query must be string");
     }
 
     if (typeof callback !== "function") {
-        throw new Error("callback must be function");
+      throw new Error("callback must be function");
     }
 
     if (callback.length !== 1) {
-        throw new Error("callback must have 1 argument");
+      throw new Error("callback must have 1 argument");
     }
 
-    rl.question(query, callback);
+    rl.question(query, () => {
+      callback();
+      rl.close();
+    });
   }
 
   static readLineAsync(query) {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
     if (arguments.length !== 1) {
-        throw new Error("arguments must be 1.");
+      throw new Error("arguments must be 1.");
     }
 
     if (typeof query !== "string") {
-        throw new Error("query must be string");
+      throw new Error("query must be string");
     }
 
     return new Promise((resolve) => {
       rl.question(query, (input) => {
+        rl.close();
         resolve(input);
       });
     });
   }
 
   static close() {
-    rl.close();
+    // rl.close();
   }
 
   static print(message) {
