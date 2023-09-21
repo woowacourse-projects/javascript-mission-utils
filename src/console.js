d73a659069dd7999f113c4854c379aa006e81432
woowapark <shinyoung.park@woowahan.com>
2023-09-21 14:35:18 +0900

    feat: add promise api to Console

--- src/console.js
+++ src/console.js
@@ -1,35 +1,56 @@
 import readline from "readline";
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
 class Console {
   constructor() {}
 
   static readLine(query, callback) {
     if (arguments.length !== 2) {
-        throw new Error("arguments must be 2.");
+      throw new Error("arguments must be 2.");
     }
 
     if (typeof query !== "string") {
-        throw new Error("query must be string");
+      throw new Error("query must be string");
     }
-        
+
     if (typeof callback !== "function") {
-        throw new Error("callback must be function");
+      throw new Error("callback must be function");
     }
 
     if (callback.length !== 1) {
-        throw new Error("callback must have 1 argument");
+      throw new Error("callback must have 1 argument");
     }
 
-    rl.question(query, callback);
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    rl.question(query, () => {
+      rl.close();
+      callback();
+    });
   }
 
-  static close() {
-    rl.close();
+  static readLineAsync(query) {
+    if (arguments.length !== 1) {
+      throw new Error("arguments must be 1.");
+    }
+
+    if (typeof query !== "string") {
+      throw new Error("query must be string");
+    }
+
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    return new Promise((resolve) => {
+      rl.question(query, (input) => {
+        rl.close();
+        resolve(input);
+      });
+    });
   }
 
   static print(message) {
