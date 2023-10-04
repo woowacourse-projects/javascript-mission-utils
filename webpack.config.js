e755851ed235128cb078a80e8662ee6dedfa53b9
woowapark <shinyoung.park@woowahan.com>
2023-10-04 10:48:14 +0900

    feature: add exports settings to support both commonjs and esm

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,12 @@
-const path = require("path");
+import path from "path";
+import { fileURLToPath } from "url";
 
-module.exports = {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const config = {
   entry: "./src/index.js",
   target: "node",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "mission-utils.js",
-    library: {
-      name: "MissionUtils",
-      type: "umd",
-    },
-    globalObject: "this",
-    clean: true,
-  },
   resolve: {
     extensions: [".js"],
   },
@@ -33,3 +27,32 @@ module.exports = {
     ],
   },
 };
+
+export default [
+  // CJS 빌드
+  {
+    ...config,
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "mission-utils.cjs",
+      library: {
+        type: "commonjs",
+      },
+    },
+  },
+  // ESM 빌드
+  {
+    ...config,
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "mission-utils.js",
+      library: {
+        type: "module",
+      },
+      chunkFormat: "module",
+    },
+    experiments: {
+      outputModule: true, // ESM 출력 모듈 활성화
+    },
+  },
+];
